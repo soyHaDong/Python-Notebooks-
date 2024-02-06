a9c00085b40956712fc943842aab2cd9f4267932
soyHaDong <hadongpark10@gmail.com>
2024-02-06 15:54:17 -0500

    modify basics.py

---
+++ python_basics/basics.py
@@ -0,0 +1,12 @@
+# Expression is anything that is tranform into one value, such as:
+
+1+1 #but it is not going to print because it hass no print statment
+
+print(1 + 1) # this is an expression
+
+print(1+ 2 * (8 / 2)) #this is a expression too
+
+
+
+
+# Values (coning from expressions) can be categorized into data types 
